6d82d12c3f78507c96f9c0869cde4314132d7a8e
pepitaLaPistolera <acrodri991@gmail.com>
2021-01-13 20:27:11 -0300

    arreglo actividadES

--- db/migrations/20201229214901-create-actividades.js
+++ db/migrations/20201229214901-create-actividades.js
@@ -2,73 +2,65 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable(
-      'Actividads',
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        espacioId: {
-          allowNull: false,
-          type: Sequelize.INTEGER,
-          references: {
-            model: 'Espacios',
-            key: 'id',
-          },
-        },
-        nombre: {
-          allowNull: false,
-          type: Sequelize.STRING,
-        },
-        fechaHoraInicio: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        fechaHoraFin: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        responsable: {
-          allowNull: false,
-          type: Sequelize.STRING,
-        },
-        dniResponsable: {
-          allowNull: false,
-          type: Sequelize.INTEGER,
-        },
-        tipoResponsable: {
-          allowNull: false,
-          type: Sequelize.STRING,
-        },
-        estado: {
-          defaultValue: true,
-          type: Sequelize.BOOLEAN,
-        },
-        requiereControl: {
-          defaultValue: false,
-          type: Sequelize.BOOLEAN,
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
+    await queryInterface.createTable('Actividades', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      espacioId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Espacios',
+          key: 'id',
         },
       },
-      {
-        esquema: 'public',
-        tableName: 'Actividads',
+      nombre: {
+        allowNull: false,
+        type: Sequelize.STRING,
       },
-      { freezeTableName: true }
-    );
+      fechaHoraInicio: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      fechaHoraFin: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      responsable: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      dniResponsable: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      tipoResponsable: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      estado: {
+        defaultValue: true,
+        type: Sequelize.BOOLEAN,
+      },
+      requiereControl: {
+        defaultValue: false,
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Actividads');
+    await queryInterface.dropTable('Actividades');
   },
 };
