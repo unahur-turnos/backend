e6e4f0bf6c385489568cef4acc8cea60f129026e
Pablo <pablogerez2000@gmail.com>
2021-01-15 15:41:49 -0300

    Se creo modelo, entidad y migración de usuario

---
+++ db/migrations/20210115171617-create-usuario.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      nombre: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      apellido: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      contrasenia: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      dni: {
+        type: Sequelize.INTEGER,
+        unique: true,
+        allowNull: false,
+      },
+      telofono: {
+        type: Sequelize.INTEGER,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Usuarios');
+  },
+};
