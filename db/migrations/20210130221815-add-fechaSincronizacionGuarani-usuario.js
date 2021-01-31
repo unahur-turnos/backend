55511c9b6ed4af8454b6eba9b0dcb7dbccd5f78c
Pablo <pablogerez2000@gmail.com>
2021-01-31 12:28:55 -0300

    Se arregló migración de fechaDeSincronizacion

--- db/migrations/20210130221815-add-fechaSincronizacionGuarani-usuario.js
+++ db/migrations/20210130221815-add-fechaSincronizacionGuarani-usuario.js
@@ -3,28 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Usuarios', 'fechaSincronizacionGuarani', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      idCarrera: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-      },
-      nombreCarrera: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-      },
-      usuarioId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'Usuarios',
-          key: 'id',
-        },
-      },
+      type: Sequelize.DATE,
     });
   },
 
