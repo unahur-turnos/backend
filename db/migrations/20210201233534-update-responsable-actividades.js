02316a426ee90451f1b6fa7bf5e38634e95517a2
Ariel Sadran <ariel.sadran@hotmail.com>
2021-02-01 21:05:06 -0300

    Permitir null sobre responsable de la actividad

---
+++ db/migrations/20210201233534-update-responsable-actividades.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('Actividades', 'dniResponsable', {
+      allowNull: true,
+      type: Sequelize.INTEGER,
+    });
+    await queryInterface.changeColumn('Actividades', 'responsable', {
+      allowNull: true,
+      type: Sequelize.STRING,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {},
+};
